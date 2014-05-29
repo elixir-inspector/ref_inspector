b96822f49357d6048da4173d1a7770b798303f6d
Marc Neudert <marc.neudert@gmail.com>
2014-05-29 15:22:40 +0200

    updates to elixir 0.13.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExReferer.Mixfile do
   def project do
     [ app:       :ex_referer,
       version:   "0.0.1",
-      elixir:    "~> 0.13.1",
+      elixir:    "~> 0.13.3",
       deps:      deps,
       deps_path: "_deps" ]
   end
