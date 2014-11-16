e083084a6854361b5f2fff3bd1223cd02c6cc639
Marc Neudert <marc.neudert@gmail.com>
2014-11-16 15:20:36 +0100

    fixes typos

--- test/mix/ex_referer/yaml.download_test.exs
+++ test/mix/ex_referer/yaml.download_test.exs
@@ -1,17 +1,14 @@
 defmodule Mix.Tasks.ExAgent.Yaml.DownloadTest do
   use ExUnit.Case, async: false
 
-  import ExUnit.CaptureIO
-
   require Logger
 
   test "forceable download" do
-    Mix.Tasks.ExRefererYaml.Download.run(["--force"])
+    Mix.Tasks.Ex_referer.Yaml.Download.run(["--force"])
 
     Mix.ExReferer.local_yaml()
       |> Path.expand()
       |> File.exists?
       |> assert
-    end
   end
 end
