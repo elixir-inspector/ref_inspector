d0b85f73c3d70239c51227f831f54b09e605b2fd
Marc Neudert <marc.neudert@gmail.com>
2015-03-26 20:18:11 +0100

    filters out results with non-unique sort keys

--- lib/ref_inspector/database.ex
+++ lib/ref_inspector/database.ex
@@ -118,6 +118,7 @@ defmodule RefInspector.Database do
     sources
     |> Enum.map( &Map.put(&1, :sort, "#{ &1.host }#{ &1.path }") )
     |> Enum.sort( &(String.length(&1[:sort]) > String.length(&2[:sort])) )
+    |> Enum.uniq( &(&1[:sort]) )
     |> Enum.map( &Map.delete(&1, :sort) )
   end
 end
