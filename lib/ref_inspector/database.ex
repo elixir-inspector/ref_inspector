3e05fa51801b98c625368e50359f16f5d4ce3a20
Marc Neudert <marc.neudert@gmail.com>
2016-02-20 14:17:55 +0100

    cleans up Database.start_link

--- lib/ref_inspector/database.ex
+++ lib/ref_inspector/database.ex
@@ -13,9 +13,9 @@ defmodule RefInspector.Database do
   @doc """
   Starts the database server.
   """
-  @spec start_link(any) :: GenServer.on_start
-  def start_link(default \\ []) do
-    GenServer.start_link(__MODULE__, default, name: __MODULE__)
+  @spec start_link() :: GenServer.on_start
+  def start_link() do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
   def init(_), do: { :ok, %State{} }
