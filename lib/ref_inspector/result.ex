a8442ac176cf3d80b29f9b453e2616f92d966334
Marc Neudert <marc.neudert@gmail.com>
2016-04-23 17:49:22 +0200

    typespecs result struct

--- lib/ref_inspector/result.ex
+++ lib/ref_inspector/result.ex
@@ -3,6 +3,13 @@ defmodule RefInspector.Result do
   Result struct.
   """
 
+  @type t :: %__MODULE__{
+    referer: String.t,
+    medium:  String.t | :unknown,
+    source:  String.t | :unknown,
+    term:    String.t | :none
+  }
+
   defstruct [
     referer: "",
     medium:  :unknown,
