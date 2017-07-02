dab19be9ada1458bb862b75ee7438d9d1ec6b4bb
Marc Neudert <marc.neudert@gmail.com>
2017-07-02 16:24:46 +0200

    removes elixir 1.0.x compatibility code

--- verify/lib/mix/tasks/ref_inspector/verify.ex
+++ verify/lib/mix/tasks/ref_inspector/verify.ex
@@ -1,25 +1,8 @@
-if Version.match?(System.version, ">= 1.0.3") do
-  #
-  # Elixir 1.0.3 and up requires mixed case module namings.
-  #
-  defmodule Mix.Tasks.RefInspector.Verify do
-    @moduledoc false
-    @shortdoc  "Verifies parser results"
+defmodule Mix.Tasks.RefInspector.Verify do
+  @moduledoc false
+  @shortdoc  "Verifies parser results"
 
-    use Mix.Task
+  use Mix.Task
 
-    defdelegate run(args), to: Mix.RefInspector.Verify
-  end
-else
-  #
-  # Elixir 1.0.2 requires the underscore module naming.
-  #
-  defmodule Mix.Tasks.Ref_inspector.Verify do
-    @moduledoc false
-    @shortdoc  "Verifies parser results"
-
-    use Mix.Task
-
-    defdelegate run(args), to: Mix.RefInspector.Verify
-  end
+  defdelegate run(args), to: Mix.RefInspector.Verify
 end
