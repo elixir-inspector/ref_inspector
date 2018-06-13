bc3cd12c21a7167f17758a0642cd40b6a357be33
Marc Neudert <marc.neudert@gmail.com>
2018-06-13 18:25:28 +0200

    restructures parse benchmark script

--- bench/parse.exs
+++ bench/parse.exs
@@ -1,28 +1,36 @@
-urlstream =
-  [__DIR__, "urls.txt"]
-  |> Path.join()
-  |> File.read!()
-  |> Code.eval_string([], file: "urls.txt")
-  |> case do
-    {urllist, _} when is_list(urllist) -> urllist
-    _ -> []
+defmodule RefInspector.Benchmark.Parse do
+  def run() do
+    [__DIR__, "urls.txt"]
+    |> Path.join()
+    |> File.read!()
+    |> Code.eval_string([], file: "urls.txt")
+    |> case do
+      {urllist, _} when is_list(urllist) -> urllist
+      _ -> []
+    end
+    |> Stream.cycle()
+    |> run_benchmark()
   end
-  |> Stream.cycle()
 
-Enum.each([2, 4, 8, 16, 32, 64, 128], fn parallel ->
-  IO.puts("Starting parallel: #{parallel}")
+  defp run_benchmark(urlstream) do
+    Enum.each([2, 4, 8, 16, 32, 64, 128], fn parallel ->
+      IO.puts("Starting parallel: #{parallel}")
 
-  {us, _} =
-    :timer.tc(fn ->
-      for _ <- 1..parallel do
-        Task.async(fn ->
-          urlstream
-          |> Enum.take(1000)
-          |> Enum.each(&RefInspector.parse/1)
+      {us, _} =
+        :timer.tc(fn ->
+          for _ <- 1..parallel do
+            Task.async(fn ->
+              urlstream
+              |> Enum.take(1000)
+              |> Enum.each(&RefInspector.parse/1)
+            end)
+          end
+          |> Enum.map(&Task.await(&1, :infinity))
         end)
-      end
-      |> Enum.map(&Task.await(&1, :infinity))
-    end)
 
-  IO.puts("done in #{us} microseconds")
-end)
+      IO.puts("done in #{us} microseconds")
+    end)
+  end
+end
+
+RefInspector.Benchmark.Parse.run()
