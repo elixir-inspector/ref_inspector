d30700cd0ffa8f54feae594c18461ed12bb13122
Marc Neudert <marc.neudert@gmail.com>
2018-07-20 20:06:44 +0200

    cleans up parsing process

--- lib/ref_inspector/parser.ex
+++ lib/ref_inspector/parser.ex
@@ -11,12 +11,13 @@ defmodule RefInspector.Parser do
   """
   @spec parse(String.t()) :: map
   def parse(ref) do
-    uri = ref |> URI.parse()
+    uri = URI.parse(ref)
+    uri = %{uri | host: uri.host || "", path: uri.path || ""}
 
     result =
-      case is_internal?(uri.host || "") do
+      case is_internal?(uri.host) do
         true -> %Result{medium: :internal}
-        false -> uri |> parse_ref()
+        false -> parse_ref(uri, Database.list())
       end
 
     %{result | referer: ref}
@@ -27,7 +28,7 @@ defmodule RefInspector.Parser do
   defp is_internal?(host) do
     sources = Application.get_env(:ref_inspector, :internal, [])
 
-    host |> String.ends_with?(sources)
+    String.ends_with?(host, sources)
   end
 
   defp maybe_parse_query(nil, _, result), do: result
@@ -41,6 +42,8 @@ defmodule RefInspector.Parser do
 
     %{result | term: term}
   end
+
+  defp match_medium(_, {_, []}), do: nil
 
   defp match_medium(ref, {medium, [source | sources]}) do
     case matches_source?(ref, source) do
@@ -57,21 +60,14 @@ defmodule RefInspector.Parser do
     end
   end
 
-  defp match_medium(_, {_, []}), do: nil
-
-  defp matches_source?(ref, source) do
-    String.ends_with?(ref.host || "", source[:host]) &&
-      String.starts_with?(ref.path || "/", source[:path])
+  defp matches_source?(%{host: ref_host, path: ref_path}, %{host: src_host, path: src_path}) do
+    String.ends_with?(ref_host, src_host) && String.starts_with?(ref_path, src_path)
   end
 
   defp parse_query(_, []), do: :none
 
   defp parse_query(query, [param | params]) do
     Map.get(query, param, parse_query(query, params))
-  end
-
-  defp parse_ref(ref) do
-    ref |> parse_ref(Database.list())
   end
 
   defp parse_ref(_, []), do: %Result{}
