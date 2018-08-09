7bf91ee2d367274351fefeed1aba69c315471d82
Marc Neudert <marc.neudert@gmail.com>
2018-08-09 18:20:16 +0200

    cleans up verification config

--- verify/config/config.exs
+++ verify/config/config.exs
@@ -3,5 +3,4 @@ use Mix.Config
 config :ref_inspector,
   database_files: ["referers.yml"],
   database_path: Path.join(__DIR__, "../database"),
-  internal: ["snowplowanalytics.com"],
-  pool: [max_overflow: 0, size: 1]
+  internal: ["snowplowanalytics.com"]
