78ea3cd15d5a529ca6e47897ab369e464770c05b
Marc Neudert <marc.neudert@gmail.com>
2018-11-10 14:55:06 +0100

    starts initializer test agent just once

--- test/ref_inspector/supervisor_test.exs
+++ test/ref_inspector/supervisor_test.exs
@@ -12,7 +12,7 @@ defmodule RefInspector.SupervisorTest do
     def get_init, do: Agent.get(__MODULE__, & &1)
   end
 
-  setup do
+  setup_all do
     init = Application.get_env(:ref_inspector, :init)
     {:ok, _} = Initializer.start_link()
 
