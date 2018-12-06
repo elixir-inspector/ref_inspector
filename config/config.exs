6316f9e94782c8438e73718f41db0a73542fabab
Marc Neudert <marc.neudert@gmail.com>
2018-12-06 19:20:23 +0100

    improves path configurations

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,13 @@
 use Mix.Config
 
 if Mix.env() == :bench do
-  config :ref_inspector, database_path: Path.join(__DIR__, "../bench/data")
+  config :ref_inspector, database_path: Path.expand("../bench/data", __DIR__)
 end
 
 if Mix.env() == :test do
   config :ref_inspector,
     database_files: ["referers_search.yml", "referers_social.yml"],
-    database_path: Path.join(__DIR__, "../test/fixtures"),
+    database_path: Path.expand("../test/fixtures", __DIR__),
     ets_cleanup_delay: 10,
     internal: ["www.example.com", "www.example.org"]
 end
--- verify/config/config.exs
+++ verify/config/config.exs
@@ -2,5 +2,5 @@ use Mix.Config
 
 config :ref_inspector,
   database_files: ["referers.yml"],
-  database_path: Path.join(__DIR__, "../database"),
+  database_path: Path.expand("../database", __DIR__),
   internal: ["snowplowanalytics.com"]
