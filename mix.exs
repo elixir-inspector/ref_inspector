2f831d6e695c135539852f468b45d7378057986f
Marc Neudert <marc.neudert@gmail.com>
2019-03-07 18:21:53 +0100

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule RefInspector.Mixfile do
 
   defp deps do
     [
-      {:benchee, "~> 0.13.0", only: :bench, runtime: false},
+      {:benchee, "~> 0.14.0", only: :bench, runtime: false},
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
