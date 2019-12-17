1ac8d8093be1a689f6c0a284882de321008a8790
Marc Neudert <marc.neudert@gmail.com>
2019-12-17 18:28:21 +0100

    Update Elixir requirement for verification tests

--- verify/mix.exs
+++ verify/mix.exs
@@ -5,7 +5,7 @@ defmodule RefInspector.Verification.MixProject do
     [
       app: :ref_inspector_verification,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       deps: [{:ref_inspector, path: "../"}],
       deps_path: "../deps",
       lockfile: "../mix.lock"
