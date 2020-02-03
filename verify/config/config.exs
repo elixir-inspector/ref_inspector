60bb9cff4cd996ab923e6190986051128d7b8268
Marc Neudert <marc.neudert@gmail.com>
2020-02-03 18:03:46 +0100

    Use referers.yml from GitHub master for verification

--- verify/config/config.exs
+++ verify/config/config.exs
@@ -3,4 +3,5 @@ use Mix.Config
 config :ref_inspector,
   database_files: ["referers.yml"],
   database_path: Path.expand("../database", __DIR__),
-  internal: ["snowplowanalytics.com"]
+  internal: ["snowplowanalytics.com"],
+  remote_urls: [{"referers.yml", "https://raw.githubusercontent.com/snowplow-referer-parser/referer-parser/master/resources/referers.yml"}]
