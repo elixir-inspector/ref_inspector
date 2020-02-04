ff6c5f1c7114dd33da16274eec5807e32bc2855f
Marc Neudert <marc.neudert@gmail.com>
2020-02-04 18:12:06 +0100

    Please the formatting gods

--- verify/config/config.exs
+++ verify/config/config.exs
@@ -4,4 +4,7 @@ config :ref_inspector,
   database_files: ["referers.yml"],
   database_path: Path.expand("../database", __DIR__),
   internal: ["snowplowanalytics.com"],
-  remote_urls: [{"referers.yml", "https://raw.githubusercontent.com/snowplow-referer-parser/referer-parser/master/resources/referers.yml"}]
+  remote_urls: [
+    {"referers.yml",
+     "https://raw.githubusercontent.com/snowplow-referer-parser/referer-parser/master/resources/referers.yml"}
+  ]
