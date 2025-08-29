91efa3ed021aac78beb2020e77f0241db348b055
Marc Neudert <marc.neudert@gmail.com>
2025-08-29 12:30:04 +0200

    Bump version to v3.0.0-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RefInspector.MixProject do
 
   @url_changelog "https://hexdocs.pm/ref_inspector/changelog.html"
   @url_github "https://github.com/elixir-inspector/ref_inspector"
-  @version "2.1.0"
+  @version "3.0.0-dev"
 
   def project do
     [
